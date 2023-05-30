e6fde772f15d7ed1c4d80062b0a4bb783b5c4181
Icaro Bernardes <asaicaro@gmail.com>
2023-05-30 14:13:29 -0300

    teste

--- www/js/builder.js
+++ www/js/builder.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     // Cria a estrutura HTML em forma de string
     let listHTML = teams.map((el) => {
-        return "<iframe class='content' id='" + el + "' src=''>";
+        return "<iframe class='content' id='" + el + "' src='" + el + ".html'>";
     });
 
     // Adiciona o placeholder sem opacidade e com atributo "src" preenchido
